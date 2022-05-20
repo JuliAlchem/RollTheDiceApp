908ffe6bc208052bbdf4982389a18e10b26c5520
JuliAlch <julialchem@gmail.com>
2022-05-20 12:09:34 +0200

    StartPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg'; // Change logo
 import './App.css';
 import Navbar from './Components/Navbar';
+import Main from './Components/Main';
 import React from 'react';
 
 function App() {
   return (
     <React.Fragment>
+      
       <Navbar/>
+      <div className='container-principle'>
+      <Main/>
+      </div>
     </React.Fragment>
   );
 }
