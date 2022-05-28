7e32bdf6de8275ca420b94fe7eb35a333f3c28bd
JuliAlch <julialchem@gmail.com>
2022-05-28 12:42:59 +0200

    RegisterDone

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import RollDice from './Components/RollDice';
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; // for navigate
 
+import axios from 'axios';
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials = true;
+
 
 function App() {
   return (
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,18 +1,22 @@
 import React, {useState}  from "react";
 import "../Styles/register.css";
+import axios from "axios";
 
-
+import swal from 'sweetalert';
+import {useNavigate} from 'react-router-dom';
 
 function Register () {
     
+    const navigate = useNavigate();
     const [registerInput, setRegister] = useState({
         name: '',
         email: '',
         password: '',
+        error_list: [],
     });
 
     const handleInput = (e) => {
-        e.presist();
+        e.persist();
         setRegister({...registerInput, [e.target.name]: e.target.value });
     }
 
@@ -23,7 +27,24 @@ function Register () {
             email: registerInput.email,
             password: registerInput.password,
         }
-        
+    
+    
+    axios.get('/sanctum/csrf-cookie').then(response => {
+        axios.post('/api/register', data).then(res => {
+            if(res.data.status === 200)
+            {
+                localStorage.setItem('auth_token', res.data.token);
+                localStorage.setItem('auth_name', res.data.username);
+                swal('Success', res.data.message, "success");
+                navigate('/');
+            }
+            else
+            {
+                setRegister({...registerInput, error_list: res.data.validation_errors});
+            }
+        });
+    });
+
     }
 
     return (
@@ -39,44 +60,49 @@ function Register () {
             <form onSubmit={registerSubmit}>
                 <label> Name: </label>
                 <input
-                    type='text'
+                    type=''
                     name ='name'
                     onChange={handleInput}
                     value={registerInput.name}
                 />
+                <span>{registerInput.error_list.name}</span>
 
                <label> Email: </label>
                 <input
-                    type='text'
+                    type=''
                     name ='email'
                     onChange={handleInput}
                     value={registerInput.email}
                 />
+                <span>{registerInput.error_list.email}</span>
 
                 <label > Password:
                 </label>
                 <input
-                    type='password'
+                    type=''
                     name='password'
                     onChange={handleInput}
                     value={registerInput.password}
-                   
                 />
+                <span>{registerInput.error_list.password}</span>
                  
                 <button className='btn' type='submit'
                 >Sign Up</button>
             </form>
             </section>
 
+             {/* 
             <section className='login'>
             <p className='login'>
                 Already registered? <br />
                 <span className='line'>
-                    {/* put router link here */}
+                   put router link here 
                     <a href='#'>Sign In</a>
                 </span>
             </p>
+           
             </section>
+             */}
         </div>
     )
 }
